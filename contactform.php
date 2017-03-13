79a1cd9de334cde021585cc1b2c6ef49a9a5111e
Daniel F. Gómez G <dafego@gmail.com>
2017-03-13 11:33:40 -0500

    ajuste captcha requerido

--- contactform.php
+++ contactform.php
@@ -22,6 +22,16 @@
 	<link href="css/estilos.css" rel="stylesheet" type="text/css"/>
 	<link rel="stylesheet" type="text/css" href="css/formstyle.css">
 	<script src='https://www.google.com/recaptcha/api.js'></script>
+	<script>
+		window.onload = function() {
+			var recaptcha = document.forms["myForm"]["g-recaptcha-response"];
+			recaptcha.required = true;
+			recaptcha.oninvalid = function(e) {
+	    // do something
+	    alert("Please complete the captcha");
+	}
+}
+</script>
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -143,12 +153,11 @@
 						<input id="sociedad" name="description[sociedad]" value="AGNOVD" type="hidden"> 
 						<input id="modified_user_id" name="fields[modified_user_id]" style="display:none" type="hidden" value="1">
 						<input id="created_by" name="fields[created_by]" style="display:none" type="hidden" value="1">
-						<input id="assigned_user_id" name="fields[assigned_user_id]" style="display:none" type="hidden" value="1">
+						<input id="assigned_user_id" name="fields[assigned_user_id]" style="display:none" type="hidden" value="81a539d4-fc5e-11e6-a4a1-02c62a0543b1">
 						<input id="sasa_relacion_c" name="fields[sasa_relacion_c]" type="hidden" value="8">
 						<input id="account_id" name="fields[account_id]" type="hidden">
 						<input id="contact_id" name="fields[contact_id]" type="hidden">
 						<input id="team_id" name="fields[team_id]" type="hidden" type="hidden" value="af6aa276-e892-11e6-8ed9-06776b78d473">
-						<input id="team_id" name="fields[assgined_user_id]" type="hidden" type="hidden" value="81a539d4-fc5e-11e6-a4a1-02c62a0543b1">	
 						<input id="status" name="fields[status]" type="hidden" type="hidden" value="New">	
 						<input id="sasa_tificacion_casos_cases_1sasa_tificacion_casos_ida" name="fields[sasa_tificacion_casos_cases_1sasa_tificacion_casos_ida]" type="hidden" type="hidden" value="0b59b2fc-8a88-11e6-8d46-06b20b8677ed">			
 						<input id="sasa_fuente_c" name="fields[sasa_fuente_c]" type="hidden" type="hidden" value="9">
